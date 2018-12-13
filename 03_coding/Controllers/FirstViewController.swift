faf47f7b6fbfc684ebb2ac09f2eb8b3ce05236c6
Jair <jair@synapsis.com.mx>
2018-12-12 20:58:53 -0600

    Agregar condicionales
    Se agregar condicionales de los datos obtenidos de la interfaz y se cambia el color de fondo.

--- 03_coding/Controllers/FirstViewController.swift
+++ 03_coding/Controllers/FirstViewController.swift
@@ -14,7 +14,10 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var sliderAger: UISlider!
     
+    @IBOutlet weak var nameText: UITextField!
+    
     var userAge : Int = -1
+    var userName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,8 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
    
         if let theText = textField.text {
-            print(theText)
+           
+            userName = theText
         }
         
         return true
@@ -41,6 +45,44 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     }
     
     
+    @IBAction func validaDatos(_ sender: UIButton) {
+        
+        
+        let puedeIngresar = ( userName == "Jair" || userAge >= 18 )
+        
+        if puedeIngresar {
+            
+            self.view.backgroundColor = UIColor( red: 105.0/255.0, green: 231.0/255.0, blue: 39.0/255.0, alpha: 0.7 )
+            
+        } else {
+            
+            self.view.backgroundColor = UIColor( red: 209.0/255.0, green: 18.0/255.0, blue: 16.0/255.0, alpha: 1 )
+            
+        }
+        
+        
+        /*
+        if userName == "Jair" {
+            
+            print("Ingresa")
+            self.view.backgroundColor = UIColor( red: 20.0/255, green: 168.0/255, blue: 247.0/255, alpha: 0.7 )
+            
+        } else {
+            if userAge > 18 {
+                
+                print("Ingresa")
+                self.view.backgroundColor = UIColor( red: 105.0/255.0, green: 231.0/255.0, blue: 39.0/255.0, alpha: 0.7 )
+                
+            } else {
+                
+                print("No puedes ingresar")
+                self.view.backgroundColor = UIColor( red: 221.0/255.0, green: 79.0/255.0, blue: 67.0/255.0, alpha: 1 )
+                
+            }
+        }
+        */
+        
+    }
     
     func updateAgeLabel() {
         

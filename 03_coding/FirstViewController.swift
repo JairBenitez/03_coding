762c1ce0b28173eac26dcfb50ac10db5d72064bb
Jair <jair@synapsis.com.mx>
2018-12-12 18:32:54 -0600

    Initial Commit

---
+++ 03_coding/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  03_coding
+//
+//  Created by Jair Benítez on 12/12/18.
+//  Copyright © 2018 Jair Benítez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 03_coding/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  03_coding
+//
+//  Created by Jair Benítez on 12/12/18.
+//  Copyright © 2018 Jair Benítez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

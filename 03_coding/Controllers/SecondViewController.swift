a26eb6ace4cec0215d721601a4bbf88b61b5e9a5
Jair <jair@synapsis.com.mx>
2018-12-13 18:27:37 -0600

    Numeros de fibonacci
    Establecer stepper, bucle for y propiedades compactMap y Joins de arrays

--- 03_coding/Controllers/SecondViewController.swift
+++ 03_coding/Controllers/SecondViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var labelStep: UILabel!
+    @IBOutlet weak var fibResult: UITextView!
+    @IBOutlet weak var steper: UIStepper!
+    
+    var fibonacci : [Int] = [0, 1]
+    var fibId = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        actualizaEtiquetas( n: Int( steper.value ) )
+        
     }
 
 
+    func genraFibNumber( ) {
+        
+        if( fibId <= 1 || fibId >= 20 ) {
+            return
+        }
+        
+        // Generar números
+        
+        fibonacci = [0, 1]
+        
+        for i in 2...fibId {
+            fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+        }
+        
+        let fibStr : [String] = fibonacci.compactMap( { String($0) } )
+        let result : String = fibStr.joined(separator: "\n")
+        fibResult.text = result
+        
+    }
+    
+    @IBAction func steperControl(_ sender: UIStepper) {
+        
+    
+        actualizaEtiquetas( n: Int( sender.value ) )
+        
+    }
+    
+    func actualizaEtiquetas(n: Int) {
+        
+        fibId = n
+        labelStep.text = "\(fibId)"
+        
+        genraFibNumber()
+    }
+    
 }
 
